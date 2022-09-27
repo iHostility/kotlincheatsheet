c77c223a9d7888504d126a51108c02d5a6985927
Denis Voloshin <dvoloshin@licard.com>
2022-09-27 17:31:29 +0300

    Initial commit

---
+++ src/main/kotlin/su/hostile/kotlincheatsheet/KotlincheatsheetApplication.kt
@@ -0,0 +1,11 @@
+package su.hostile.kotlincheatsheet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlincheatsheetApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlincheatsheetApplication>(*args)
+}
---
+++ src/test/kotlin/su/hostile/kotlincheatsheet/KotlincheatsheetApplicationTests.kt
@@ -0,0 +1,13 @@
+package su.hostile.kotlincheatsheet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlincheatsheetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
